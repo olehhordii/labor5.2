781822cb457f13ebb8279ba927034eab31b1d554
olehhordii <oleh.hordii.ik.2021@lpnu.ua>
2021-11-04 00:59:43 +0200

    First commit master

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../labor5.2/labor5.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			int c = 1.5;
+			int t;
+			t = A(1, 2, 3);
+			Assert::AreEqual(t, c);
+		}
+	};
+}
